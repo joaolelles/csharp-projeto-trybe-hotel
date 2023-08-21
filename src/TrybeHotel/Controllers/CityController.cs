1b00b2cb3de76aba68b652ea63fb1c109413b584
João <joaolelles7@gmail.com>
2023-08-21 18:09:26 -0300

    Iniciando o projeto

--- src/TrybeHotel/Controllers/CityController.cs
+++ src/TrybeHotel/Controllers/CityController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using TrybeHotel.Models;
 using TrybeHotel.Repository;
-
+// 
 namespace TrybeHotel.Controllers
 {
     [ApiController]
@@ -13,16 +13,18 @@ namespace TrybeHotel.Controllers
         {
             _repository = repository;
         }
-        
+
         // 2. Desenvolva o endpoint GET /city
         [HttpGet]
-        public IActionResult GetCities(){
+        public IActionResult GetCities()
+        {
             throw new NotImplementedException();
         }
 
         // 3. Desenvolva o endpoint POST /city
         [HttpPost]
-        public IActionResult PostCity([FromBody] City city){
+        public IActionResult PostCity([FromBody] City city)
+        {
             throw new NotImplementedException();
         }
     }
